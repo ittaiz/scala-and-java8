49f94c009089d8cae9a5f72566019d48b8ec302b
ittaiz <ittaiz@wix.com>
2016-09-18 20:00:33 -0700

    apparently SO was wrong, java8 does have built in Function2

--- src/main/scala/com/ittaiz/javaone/type_inference/functions/JavaExample.java
+++ src/main/scala/com/ittaiz/javaone/type_inference/functions/JavaExample.java
@@ -1,16 +1,14 @@
 package com.ittaiz.javaone.type_inference.functions;
 
+import java.util.function.BiFunction;
+
 public class JavaExample {
     public static void main(String[] args) {
 
-        Function2<Integer, Integer, Integer> adder = (a, b) -> a + b;
+        BiFunction<Integer, Integer, Integer> adder = (a, b) -> a + b;
 
         System.out.println(adder.apply(5, 7));
     }
 
-    @FunctionalInterface
-    interface Function2 <A, B, R> {
-        public R apply (A a, B b);
-    }
 }
 
